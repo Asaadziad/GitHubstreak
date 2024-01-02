1f64f5acc707a8742f361dfc8fb3d7fa67b48831
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-01-02 07:37:23 +0000

    updated the ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {useState} from 'react'
 import UserTable from './components/UserTable';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Card from './components/Card.js';
 import Signup from './components/Signup';
 
 export default function App() {
   const [userCount , setUserCount] = useState(0);
   return (
     <main>
-    <Header/>
+    <Navbar/> 
+    <Card/>
+    {/* <Header/> */}
     <Signup userCount={[userCount,setUserCount]}/>
     <UserTable userCount={userCount}/>
     </main>
