ed85be451c38830580e1e5743860b925bafec4b4
Asaadziad <asaadabuelhija@gmail.com>
2024-01-02 20:14:27 +0200

    fixed performance stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useState} from 'react'
 import UserTable from './components/UserTable';
 import Navbar from './components/Navbar';
@@ -7,6 +7,9 @@ import Signup from './components/Signup';
 
 export default function App() {
   const [userCount , setUserCount] = useState(0);
+  useEffect(() => {
+    localStorage.setItem('cacheUsers', JSON.stringify([]));
+  }, []);
   return (
     <main>
     <Navbar/> 
