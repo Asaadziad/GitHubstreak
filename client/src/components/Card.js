edd952f3484d599352ed6f1ee105436f69cd1af2
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-01-03 19:59:27 +0000

    Update Card.js

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,7 +4,7 @@ export default function WelcomeCard() {
   return (
     <header className="bg-zinc-300 self-stretch flex flex-col items-stretch mt-4 pt-9 pb-8 px-3.5 rounded-xl m-5 shadow-xl" >
       <div className="text-black text-center text-3xl whitespace-nowrap " aria-label="Welcome message">
-        Welcome to GitHubstreak
+        Welcome to Githubstreak 
       </div>
     </header>
   );
